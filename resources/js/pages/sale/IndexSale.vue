ad64a4ae6837127138e4cbb65b03814773ee0f03
strelok93 <strelok0493@gmail.coom>
2023-01-14 02:28:20 +0500

    f

--- resources/js/pages/sale/IndexSale.vue
+++ resources/js/pages/sale/IndexSale.vue
@@ -5,30 +5,23 @@
         </div>
         <ListProducts 
             v-if="PageData.listProducts.length"
-            @close="PageData.listProducts = []"
+            @close="closeListProducts"
             :setProductId="setProductId" 
             :listProducts="PageData.listProducts"
         />
-        <main>
-            <button @click="setProductId(10)" class="bg-gray-300 px-3 mr-2 rounded-sm shadow-sm">Product 10</button>
-            <button @click="setProductId(85)" class="bg-gray-300 px-3 mr-2 rounded-sm shadow-sm">Product 85</button>
-            <button @click="setProductId(86)" class="bg-gray-300 px-3 mr-2 rounded-sm shadow-sm">Product 86</button>
-            <button @click="setProductId(87)" class="bg-gray-300 px-3 mr-2 rounded-sm shadow-sm">Product 87</button>
-            <button @click="setProductId(94)" class="bg-gray-300 px-3 mr-2 rounded-sm shadow-sm">Product 94</button>
-        </main>
-
     </section>
 </template>
 
 <script setup lang="ts">
-import axios from '../../modules/axios';
-import { reactive ,watch, computed  } from 'vue';
+import axios from '../../modules/axios'
+import { reactive , watch , computed  } from 'vue'
 import ListProducts from './components/ListProducts.vue'
 const PageData = reactive({
     code: "",
     reading: false,
     textInBarcode: null,
     listProducts: [],
+    blocker: true,
 })
 
 const textinBarcode = computed(() => PageData.textInBarcode)
@@ -36,19 +29,35 @@ const textinBarcode = computed(() => PageData.textInBarcode)
 watch(textinBarcode , (currentValue) => {
     if(currentValue == null) return
     
-    const productId = currentValue.replace('product','');
-    const contain = PageData.listProducts.find((product) => product.id == productId)
-    
-    if(contain) {
-        if(contain.count > contain.totalCount) contain.totalCount++
-        return
+    if(PageData.blocker){
+        
+        PageData.blocker = false
+        // textdan IDni ajratib olamiz
+        const productId = currentValue.replace('product','')
+        const product = PageData.listProducts.find(product => product.id == productId)
+
+        // Agar bor bo'lsa
+        if(product) {
+            if(product.count > product.totalCount) product.totalCount++
+            return PageData.blocker = true
+        }
+
+
+
+        // yangi tovar bolsa
+        axios.get(`products/${productId}`).then((res) => {
+            PageData.blocker = true
+            res.data.totalCount = 1
+            PageData.listProducts.push(res.data)
+        })
     }
-    axios.get(`products/${productId}`).then((res) => {
-        res.data.totalCount = 1
-        PageData.listProducts.push(res.data)
-    })
 })
 
+
+function closeListProducts(){
+    PageData.listProducts = []
+    PageData.blocker = true
+}
 
 
 function setProductId(id){
@@ -65,12 +74,12 @@ document.addEventListener('keypress', event => {
 
         }
     } 
-    else PageData.code += event.key; //while this is not an 'enter' it stores the every key            
+    else PageData.code += event.key //while this is not an 'enter' it stores the every key            
     
 
     //run a timeout of 200ms at the first read and clear everything
     if (!PageData.reading) {
-        PageData.reading = true;
+        PageData.reading = true
         setTimeout(() => {
             PageData.textInBarcode = PageData.code
 
@@ -78,7 +87,7 @@ document.addEventListener('keypress', event => {
             
             PageData.code = ""
             PageData.reading = false
-        }, 200);  //200 works fine for me but you can adjust it
+        }, 100)  //200 works fine for me but you can adjust it
     }
-});
+})
 </script>
--- resources/js/pages/sale/components/ListProducts.vue
+++ resources/js/pages/sale/components/ListProducts.vue
@@ -7,70 +7,60 @@
                 </button>
             </header>
             <main class="flex-grow overflow-hidden overflow-y-auto list-products shadow-inner px-3">
-                <table class="w-full text-center">
+                <table class="w-full text-center mt-4">
                     <tr>
-                        <td class="py-1">Name</td>
-                        <td class="py-1">Size</td>
-                        <td class="py-1">Price</td>
-                        <td class="py-1">Count</td>
-                        <td class="py-1">Storehouse count</td>
-                        <td class="py-1">Total price</td>
+                        <td class="py-2 font-semibold">Name</td>
+                        <td class="py-2 font-semibold">Size</td>
+                        <td class="py-2 font-semibold">Price</td>
+                        <td class="py-2 font-semibold">Count</td>
+                        <td class="py-2 font-semibold">Store count</td>
+                        <td class="py-2 font-semibold">Total price</td>
+                        <td class="py-2 font-semibold"></td>
                     </tr>
-                    <tr v-for="product in listProducts">
-                        <td class="border-y py-5">{{ product.product_names.name }}</td>
-                        <td class="border-y py-5">{{ product.size.name }}</td>
-                        <td class="border-y py-5">{{ product.price }}</td>
-                        <td class="border-y py-5">
-                            <button @click="decrement(product)" class="px-3 bg-gray-300 rounded-sm shadow w-10">
-                                -
-                            </button>
-                            {{ product.totalCount }}
-                            <button @click="increment(product)" class="px-3 bg-gray-300 rounded-sm shadow w-10">
-                                +
-                            </button>
+                    <tr v-for="(product, index) in listProducts" :key="index">
+                        <td class="border-y py-3">{{ product.product_names.name }}</td>
+                        <td class="border-y py-3">{{ product.size.name }}</td>
+                        <td class="border-y py-3">{{ product.price }}</td>
+                        <td class="border-y py-3 ">
+                            <div class="flex items-center justify-center">
+                                <button 
+                                    :disabled="product.totalCount == 1" 
+                                    :class="{'text-gray-300 hover:bg-inherit': product.totalCount == 1}" 
+                                    @click="decrement(product)" 
+                                    class="px-3 py-0.5 hover:bg-gray-100 active:bg-gray-200 rounded-sm w-10"
+                                >
+                                    <i class="fal fa-angle-left relative top-px"></i>
+                                </button>
+                                <span class="mx-4">
+                                    {{ product.totalCount }}
+                                </span>
+                                <button 
+                                    :disabled="product.totalCount == product.count" 
+                                    :class="{'text-gray-300 hover:bg-inherit': product.totalCount == product.count}" 
+                                    @click="increment(product)" 
+                                    class="px-3 py-0.5 hover:bg-gray-100 active:bg-gray-200 rounded-sm w-10"
+                                >
+                                    <i class="fal fa-angle-right relative top-px"></i>
+                                </button>
+                            </div>
                         </td>
-                        <td class="border-y py-5">{{ product.count }}</td>
-                        <td class="border-y py-5">{{ product.price * product.totalCount }}</td>
+                        <td class="border-y py-3">{{ product.count }}</td>
+                        <td class="border-y py-3">{{ product.price * product.totalCount }}</td>
+                        <td @click="deleteProduct(index)" class="border-y py-3 px-3 text-red-600 cursor-pointer hover:bg-gray-100 active:bg-slate-200">
+                            <i class="fal fa-times"></i>
+                        </td>
                     </tr>
                     <tr>
                         <td class="py-5" colspan="5"></td>
                         <td class="py-5">{{ totalPrice }}</td>
+                        <td class="py-5"></td>
                     </tr>
                 </table>
-                <div>
-                    
-                </div>
-                <!-- <div v-for="product in listProducts" class="border-b px-3 py-2 mb-2">
-                    <aside>
-                        ID: {{ product.id }}
-                    </aside>
-                    <main class="font-bold">
-                         <span class="ml-5 font-normal">Size: {{ product.size.name }}</span>
-                    </main>
-    
-                    <div>
-                        Price: {{ product.price }}
-                    </div>
-                    <div class="flex">
-                        <button @click="decrement(product)" class="px-3 bg-gray-300 rounded-sm shadow w-10">
-                            -
-                        </button>
-                        <main class="mx-3">
-                            <b>Count</b>: {{ product.totalCount }} <br>
-                        </main>
-                        <button @click="increment(product)" class="px-3 bg-gray-300 rounded-sm shadow w-10">
-                            +
-                        </button>
-                    </div>
-                    <span class="ml-6">storehouse : {{ product.count }}</span>  
-                </div> -->
             </main>
-            <footer class="bg-white p-3 border-t">
-                <button @click="setProductId(10)" class="bg-gray-300 px-3 mr-3 rounded-sm shadow-sm">Product 10</button>
-                <button @click="setProductId(85)" class="bg-gray-300 px-3 mr-3 rounded-sm shadow-sm">Product 85</button>
-                <button @click="setProductId(86)" class="bg-gray-300 px-3 mr-3 rounded-sm shadow-sm">Product 86</button>
-                <button @click="setProductId(87)" class="bg-gray-300 px-3 mr-3 rounded-sm shadow-sm">Product 87</button>
-                <button @click="setProductId(94)" class="bg-gray-300 px-3 mr-3 rounded-sm shadow-sm">Product 94</button>
+            <footer class="bg-white p-3 border-t text-right">
+                <button class="py-1 px-3 bg-gray-200 shadow border-b-2 border-pink-500 active:bg-gray-300">
+                    Sell the product
+                </button>
             </footer>
         </main>
     </section>
@@ -85,16 +75,16 @@ const totalPrice = computed(() => {
 })
 
 function decrement(product){
-    if(product.totalCount > 1){
-        product.totalCount--
-    }
+    if(product.totalCount > 1) product.totalCount--
+    
 }
 
 function increment(product){
-    // if(product.totalCount < product.count){
-        product.totalCount++
-    // }
+    if(product.totalCount < product.count) product.totalCount++
 }
 
 
+function deleteProduct(index){
+    listProducts.splice(index,1)
+}
 </script>

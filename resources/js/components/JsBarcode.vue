0f3177f17f0b2703391b0f4bf8326b032ea0ff99
strelok93 <strelok0493@gmail.com>
2023-04-26 08:53:01 +0500

    product

--- resources/js/components/JsBarcode.vue
+++ resources/js/components/JsBarcode.vue
@@ -1,5 +1,6 @@
 <template>
-    <div>
+    <div class="relative">
+        <p class="text-[7px] absolute top-0.5 left-0.5 z-50 leading-none text-gray-500">Ruzzifer</p>
         <svg :id="svgId" class="w-full"></svg>
         <p class="px-1 flex justify-between items-center text-xs">
             <span>{{ productName.name }}</span> <span>{{ product.size.name }}</span>
@@ -13,10 +14,10 @@
 import { onMounted, ref } from 'vue'
 import JsBarcode from 'JsBarcode'
 const { productName, product } = defineProps(['productName','product'])
-const textForBarcode = window.location.host + `/product/${product.id}`
+// const textForBarcode = window.location.host + `/product/${product.id}`
 
 const svgId = ref(`product${product.id}`)
 onMounted(() => {
-    JsBarcode(`#${svgId.value}`, textForBarcode, {width: 0.5,height: 50,displayValue: false});
+    JsBarcode(`#${svgId.value}`, svgId.value, {width: 1,height: 50,displayValue: false});
 })
 </script>
